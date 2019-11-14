d3506a6a1ef7277e4c47294be54fcbe7f89db1a4
cacauvicosa <cacauvicosa@gmail.com>
2019-11-14 11:20:52 -0300

    multiplicacao asm pipeline

---
+++ pipeline/codigos_exemplo/multiplicacao/multnop.asm
@@ -0,0 +1,13 @@
+lw $t3,8($zero) # t3 = m[8]
+lw $t2,4($zero) # t2 = m[4]
+addi $t1,$zero,0 # t1 resultado = 0
+nop
+loop: beq $t3,$zero,fim # $t3 == 0
+addi $t3,$t3,-1 # t3 --
+add $t1,$t1,$t2 # m = t2+....
+nop
+beq $zero,$zero,loop # soma mais um termo
+nop
+nop
+nop
+fim: sw $t1,0($zero) # grava resultado
