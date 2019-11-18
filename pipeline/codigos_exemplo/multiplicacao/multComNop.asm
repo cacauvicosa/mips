f7b84c4c77b38efffc10d75085ceaf84d7ccab6e
cacauvicosa <cacauvicosa@gmail.com>
2019-11-18 12:32:37 -0300

    trace

--- pipeline/codigos_exemplo/multiplicacao/multComNop.asm
+++ pipeline/codigos_exemplo/multiplicacao/multComNop.asm
@@ -1,8 +1,20 @@
 addi $t1,$zero,0 # t1 resultado = 0
 lw $t2,4($zero) # t2 = m[4]
 lw $t3,8($zero) # t3 = m[8]
+nop
+nop
+nop
 loop: add $t1,$t1,$t2 # m = t2+....
 addi $t3,$t3,-1 # t3 --
+nop
+nop
+nop
 beq $t3,$zero,fim # $t3 == 0
+nop
+nop
+nop
 beq $zero,$zero,loop # soma mais um termo
+nop
+nop
+nop
 fim: sw $t1,0($zero) # grava resultado
