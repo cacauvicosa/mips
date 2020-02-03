cf1a038df58af4e2009553f5f9f1d148a842f84b
canesche <michael.canesche@gmail.com>
2020-02-03 09:36:11 -0300

    adding json riscv

--- michael/riscv/riscv-single/riscv-single.v
+++ michael/riscv/riscv-single/riscv-single.v
@@ -38,7 +38,7 @@ endmodule
 
 module decode (input [31:0] inst, writedata, input clk, output [31:0] data1, data2, ImmGen, output alusrc, memread, memwrite, memtoreg, branch, output [1:0] aluop, output [9:0] funct);
   
-  wire branch, memread, memtoreg, MemWrite, regdst, alusrc, regwrite;
+  wire branch, memread, memtoreg, MemWrite, alusrc, regwrite;
   wire [1:0] aluop; 
   wire [4:0] writereg, rs1, rs2, rd;
   wire [6:0] opcode;
@@ -51,16 +51,15 @@ module decode (input [31:0] inst, writedata, input clk, output [31:0] data1, dat
   assign rd     = inst[11:7];
   assign funct = {inst[31:25],inst[14:12]};
 
-  ControlUnit control (opcode, inst, regdst, alusrc, memtoreg, regwrite, memread, memwrite, branch, aluop, ImmGen);
+  ControlUnit control (opcode, inst, alusrc, memtoreg, regwrite, memread, memwrite, branch, aluop, ImmGen);
   
   Register_Bank Registers (clk, regwrite, rs1, rs2, rd, writedata, data1, data2); 
 
 endmodule
 
-module ControlUnit (input [6:0] opcode, input [31:0] inst, output reg regdst, alusrc, memtoreg, regwrite, memread, memwrite, branch, output reg [1:0] aluop, output reg [31:0] ImmGen);
+module ControlUnit (input [6:0] opcode, input [31:0] inst, output reg alusrc, memtoreg, regwrite, memread, memwrite, branch, output reg [1:0] aluop, output reg [31:0] ImmGen);
 
   always @(opcode) begin
-    regdst   <= 0;
     alusrc   <= 0;
     memtoreg <= 0;
     regwrite <= 0;
@@ -71,7 +70,6 @@ module ControlUnit (input [6:0] opcode, input [31:0] inst, output reg regdst, al
     ImmGen   <= 0; 
     case(opcode) 
       7'b0110011: begin // R type == 51
-        regdst   <= 1;
         regwrite <= 1;
         aluop    <= 2;
 			end
